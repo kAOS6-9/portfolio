f556be9d1c7882692b245f24f6c2655ec992b53d
Krunal "kAOS" Shinde <kaostics12321@gmail.com>
2023-12-05 12:35:14 +0530

    unused files removed

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,26 +10,6 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import website from './website.png'
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 // work experiences
 import watermelon from "./company/watermelon.png";
 import spanrig from "./company/spanrig.png";
@@ -44,9 +24,6 @@ import euman from "./company/euman.png"
 import sun from "./company/sun.png"
 import tiyash from "./company/tiyash.png"
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import gaming from "./gaming.png"
 import fitness from './fitness.png'
 import tiyashport from './tiyashport.png'
@@ -92,24 +69,4 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,32 +3,11 @@ import {
     backend,
     creator,
     web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
     capgemini,
     watermelon,
     hamiters,
     spanrig,
     analysed,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
-    figmalogo,
     gaming,
     fitness,
     tiyashport,
@@ -36,7 +15,6 @@ import {
     skintest,
     equalstwova,
     anxioport,
-    website,
     mother,
     guide,
     tshirt,
@@ -74,61 +52,6 @@ import {
     {
       title: "Gamer",
       icon: creator,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
     },
   ];
   
@@ -197,33 +120,6 @@ import {
         "Collaborated closely with development teams to ensure accurate implementation of designs, maintaining design integrity throughout the development process.",
         "Reduced user onboarding time by 50% through intuitive UI enhancements in Anxio, a mental wellness mobile application.",
       ],
-    },
-  ];
-  
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
   
@@ -501,4 +397,4 @@ import {
     
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, experiences, projects };
